3f096716bdaad21eee23c0d32d03a112068ec40a
Darma Wiryanata <darmawiryanata@gmail.com>
2019-04-09 10:42:38 +0800

    change 2
    tambah desain login

--- app/src/main/java/com/example/latihan5_loginform/MainActivity.java
+++ app/src/main/java/com/example/latihan5_loginform/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.latihan5_loginform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    String stUsername, stPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.txtUsername);
+        password = (EditText)findViewById(R.id.txtPassword);
+    }
+
+    public void toastMsg(String msg) {
+
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        stUsername = username.getText() + "";
+        stPassword = password.getText() + "";
+        toastMsg("Selamat datang "+stUsername+"!");
     }
 }
